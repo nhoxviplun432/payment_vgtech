4d6a498c36411a5790f8177d0d49eccb10c589b5
nhoxviplun432 <truong.hoaigiang@tangiaba.com>
2025-09-08 22:41:28 +0700

    Initial commit: upload project code

---
+++ payment_vgtech.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @wordpress-plugin
+ * Plugin Name: Thanh toán Vgtech
+ * Description: Quản lý tài khoản tra cứu, quản lý order và thanh toán qr với VGTech
+ * Version: 1.0.0
+ * Author: hoaigiangtf@gmail.com
+ */
+
+if (!defined('WPINC')) {
+    die;
+}
+
